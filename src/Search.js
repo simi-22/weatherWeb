2a17432daa4bf54a264e2e5eff7bc714c0dffa58
simi-22 <ssimii0222@gmail.com>
2024-03-23 23:05:54 +0900

    weather-build

--- src/Search.js
+++ src/Search.js
@@ -4,7 +4,7 @@ const search = () => {
   return (
     <div id='search'>
         <input type='text' placeholder='지역을 입력하세요.'/>
-        <button>검색</button>
+        <button>검색/</button>
     </div>
   )
 }
